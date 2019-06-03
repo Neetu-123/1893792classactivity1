384c084444f12f246ff55572b27b2987d302e849
Neetu Dhingra <neetudhingra92@gmail.com>
2019-06-03 14:37:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/temp/a1893792classactivity1/exam.java
@@ -0,0 +1,13 @@
+package com.example.temp.a1893792classactivity1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class exam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
